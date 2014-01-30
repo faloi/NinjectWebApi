3238141c873cb5ef0777c6e65240d4f6e1d79a6b
Federico Aloi <federico.aloi@gmail.com>
2014-01-30 16:09:30 -0300

    Created filter to return a friendly message when an excepcion is thrown by a controller

---
+++ NinjectWebApi/Controllers/ErrorController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Web.Http;
+using NinjectWebApi.Filters;
+
+namespace NinjectWebApi.Controllers
+{
+	[InternalServerErrorFilter]
+	public class ErrorController : ApiController
+	{
+		public string Get()
+		{
+			throw new ApplicationException();
+		}
+	}
+}
---
+++ NinjectWebApi/Filters/InternalServerErrorFilter.cs
@@ -0,0 +1,17 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Web.Http.Filters;
+
+namespace NinjectWebApi.Filters
+{
+	public class InternalServerErrorFilter : ExceptionFilterAttribute
+	{
+		public override void OnException(HttpActionExecutedContext actionExecutedContext)
+		{
+			actionExecutedContext.Response = new HttpResponseMessage(HttpStatusCode.InternalServerError)
+			{
+				ReasonPhrase = "Oops, something went bad :("
+			};
+		}
+	}
+}
